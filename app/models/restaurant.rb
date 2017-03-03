cd25ae100c051f771b630805acd062591cd20016
laura crief <crieflaura@gmail.com>
2017-03-03 15:03:04 +0100

    rails new

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,7 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  #Validation
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], allow_blank: false }
+  validates :name, uniqueness: true, presence: true, allow_blank: false
+  validates :address, presence: {message: "Il manque une adresse"}
 end
